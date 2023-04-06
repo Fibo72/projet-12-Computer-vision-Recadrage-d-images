13754a3f71b053b61c12234dca875342baec77da
Thibault Lambert <thibault.lambert@etu.minesparis.psl.eu>
2023-04-06 14:30:36 +0200

    test méthode icp

---
+++ Thibault/cpd.py
@@ -0,0 +1,16 @@
+import  pycpd as cpd
+
+# create a RigidRegistration object
+
+reg = RigidRegistration(X=target, Y=source)
+
+# run the registration & collect the results
+
+TY, (s_reg, R_reg, t_reg) = reg.register()
+
+# TY is the transformed source points
+# the values in () are the registration parameters.
+# In this case of rigid registration they are:
+#     s_reg the scale of the registration
+#     R_reg the rotation matrix of the registration
+#     t_reg the translation of the registration
