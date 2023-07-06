5db8b146fdff914edb61fa0ccc914ddb9f99364c
Aymeric Cardot <aymericardot@gmail.com>
2023-07-06 11:25:48 +0200

    final moi

---
+++ test.py
@@ -0,0 +1,11 @@
+from Aymeric.decode_img import *
+
+if (__name__ == '__main__'):
+    img = get_img('data//CALSPAR16C_init-to-d7//CALSPAR16C_d1_image1-5x.dat', 'phase')
+    print(img.shape)
+    pass
+
+
+
+def UwU():
+    print('UwU')
